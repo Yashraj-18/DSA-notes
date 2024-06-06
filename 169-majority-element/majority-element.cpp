49ee1f4b2a58617c124a5ae52a9e74bbbb471908
Yashraj-18 <113794165+Yashraj-18@users.noreply.github.com>
2024-06-06 15:40:48 +0530

    Time: 17 ms (59.48%) | Memory: 26.8 MB (39.33%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,18 +1,32 @@
-//here we use the concept of hasing where we store key(the element) and value( its occurence)
-// first = element second = no. of occurence
+//moore's voting algo
 
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int , int>mpp ;
-        for(int i=0 ; i<nums.size() ; i++){
-            mpp[nums[i]]++ ;
+        int cnt = 0; int ele;
+        for(int i = 0 ; i< nums.size() ; i++){
+                    if(cnt == 0){
+                        cnt =1;
+                    ele = nums[i];
         }
-        for(auto it: mpp){
-            if(it.second > (nums.size() / 2)){
-                return it.first;
-            }
+        else if(nums[i] == ele){
+            cnt++;
         }
-        return -1;
+        else{
+            cnt--;
+        }
+     }    
+       int cnt1=0;
+       for(int i=0 ; i<nums.size() ; i++){
+        if(nums[i]==ele){
+            cnt1++;
+        }
+        if(cnt1 > (nums.size()/2 ) ){
+            return ele;
+        }
+   
+       
+    }
+    return -1;
     }
 };
