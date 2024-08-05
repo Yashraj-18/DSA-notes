c61365434227ad859fd7d20908731bff2eaa227d
Yashraj-18 <113794165+Yashraj-18@users.noreply.github.com>
2024-08-04 22:31:58 -0800

    Time: 0 ms (100.00%) | Memory: 8 MB (27.79%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if (s.size() != goal.size()) {
-            return false;
+        
+        int len=s.length();
+        for(int i=0;i<len;i++)
+        {
+            s+=s[i];
+            string sub=s.substr(i+1,len+1);
+            if(sub==goal)
+            {
+                return true;
+            }
         }
-
-        // Concatenate s to itself to create all possible rotations
-        string newString = s + s;
-
-        // Check if goal is a substring of newString
-        return newString.find(goal) != string::npos;
+        return false;
     }
 };
-// method 2
-// class Solution {
-// public:
-//     bool rotateString(string s, string goal) {
-        
-//         int len=s.length();
-//         for(int i=0;i<len;i++)
-//         {
-//             s+=s[i];
-//             string sub=s.substr(i+1,len+1);
-//             if(sub==goal)
-//             {
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-// };
