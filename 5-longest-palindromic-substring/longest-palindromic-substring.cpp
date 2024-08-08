da9c3f736e8b62d9c78621c89f398daaf7a3b29b
Yashraj-18 <113794165+Yashraj-18@users.noreply.github.com>
2024-08-08 00:24:38 -0800

    Time: 10 ms (86.77%) | Memory: 8.4 MB (76.86%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,46 +1,41 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if(s.length() <= 1) return s;
-        int max_len = 1;
         int n = s.length();
+        if (n <= 1) return s;
+        
+        int max_len = 1;
         int st = 0, end = 0;
-        
-        // Odd length
-        for(int i = 0; i < n-1; ++i){
+
+        for (int i = 0; i < n - 1; i++) {
+            // Odd length palindrome
             int l = i, r = i;
-            while(l >= 0 && r < n){
-                if(s[l] == s[r]){
-                    l--; r++;
-                }else
-                    break;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                l--;
+                r++;
             }
-            int len = r-l-1;
-            if(len > max_len){
+            int len = r - l - 1;
+            if (len > max_len) {
                 max_len = len;
-                st = l+1;
-                end = r-1;
+                st = l + 1;
+                end = r - 1;
+            }
+            
+            // Even length palindrome
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                l--;
+                r++;
+            }
+            len = r - l - 1;
+            if (len > max_len) {
+                max_len = len;
+                st = l + 1;
+                end = r - 1;
             }
         }
-        
-        // Even length
-        for(int i = 0; i < n-1; ++i){
-            int l = i, r = i+1;
-            while(l >= 0 && r < n){
-                if(s[l] == s[r]){
-                    l--; r++;
-                }else
-                    break;
-            }
-            int len = r-l-1;
-            if(len > max_len){
-                max_len = len;
-                st = l+1;
-                end = r-1;
-            }
-        }
-        
+
         return s.substr(st, max_len);
-        
     }
 };
